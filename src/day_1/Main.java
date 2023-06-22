0f694c08407e5a3ef3e53de3ca64d24010a17b32
danielOuattara <daniel.ouattara@gmx.com>
2023-06-22 08:56:51 +0200

    day 1

---
+++ src/day_1/Main.java
@@ -0,0 +1,28 @@
+package day_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String  name = in.nextLine();
+        System.out.println("Welcome " + name + "!");
+
+        //
+        int x = 5;
+        System.out.println(x);
+        int y;
+        y = 34;
+        System.out.println(y);
+        //
+
+        char t= 't';
+        System.out.println(t);
+
+
+
+
+    }
+}
